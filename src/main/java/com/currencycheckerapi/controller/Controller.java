1817c7a6cab8ad828ed5c05d4d1a005c80f059d8
Oleksii_Kovetskyi <Alexey007gold@mail.ru>
2019-10-14 21:21:58 +0300

    set response media type to JSON

--- src/main/java/com/currencycheckerapi/controller/Controller.java
+++ src/main/java/com/currencycheckerapi/controller/Controller.java
@@ -2,6 +2,7 @@ package com.currencycheckerapi.controller;
 
 import com.currencycheckerapi.service.RateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ public class Controller {
     @Autowired
     private RateService rateService;
 
-    @GetMapping("rate/{provider}/{currencyA}/{currencyB}")
+    @GetMapping(value = "rate/{provider}/{currencyA}/{currencyB}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String getRate(@PathVariable("provider") String provider,
                           @PathVariable("currencyA") String currencyA, @PathVariable("currencyB") String currencyB) {
         return rateService.getRate(provider, currencyA, currencyB);
