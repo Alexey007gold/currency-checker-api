dec1c85580615c8feecb4193345d8e17e3e31e5c
Oleksii_Kovetskyi <Alexey007gold@mail.ru>
2019-10-14 21:06:18 +0300

    Fixed resource reading

--- src/main/java/com/currencycheckerapi/service/CurrencyCodeService.java
+++ src/main/java/com/currencycheckerapi/service/CurrencyCodeService.java
@@ -3,9 +3,10 @@ package com.currencycheckerapi.service;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.List;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Map;
 
 import static java.util.stream.Collectors.toMap;
@@ -16,8 +17,8 @@ public class CurrencyCodeService {
     private Map<String, Integer> nameToNumCode;
 
     public CurrencyCodeService() throws IOException {
-        List<String> lines = Files.readAllLines(new ClassPathResource("codes.txt").getFile().toPath());
-        nameToNumCode = lines.stream()
+        InputStream inputStream = new ClassPathResource("codes.tsv").getInputStream();
+        nameToNumCode = new BufferedReader(new InputStreamReader(inputStream)).lines()
                 .map(l -> l.split("\t"))
                 .collect(toMap(a -> a[1], a -> Integer.parseInt(a[2])));
     }
