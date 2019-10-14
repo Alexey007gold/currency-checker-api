592fe971e06faa58dd0fc68e751e1c95edc403dc
Oleksii_Kovetskyi <Alexey007gold@mail.ru>
2019-10-14 22:05:16 +0300

    do not include null values in the output

--- src/main/java/com/currencycheckerapi/CurrencyCheckerApiApplication.java
+++ src/main/java/com/currencycheckerapi/CurrencyCheckerApiApplication.java
@@ -1,7 +1,10 @@
 package com.currencycheckerapi;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -10,6 +13,13 @@ public class CurrencyCheckerApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyCheckerApiApplication.class, args);
+	}
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+		return objectMapper;
 	}
 
 }
