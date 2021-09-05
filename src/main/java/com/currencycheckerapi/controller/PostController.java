7fd813de50db974d25877c41408b2ed626c68def
Oleksii_Kovetskyi <Alexey007gold@mail.ru>
2021-09-05 20:10:06 +0300

    Added a controller to log any post request body

--- src/main/java/com/currencycheckerapi/controller/PostController.java
+++ src/main/java/com/currencycheckerapi/controller/PostController.java
@@ -2,7 +2,9 @@ package com.currencycheckerapi.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PostController {
 
     @PostMapping("/post")
